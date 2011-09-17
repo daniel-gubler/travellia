75814df03aa65a000affd955d407f962079de338
Daniel Gubler <daniel.gubler@winwebexperts.com>
2011-09-17 17:19:22 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travellia::Application.config.secret_token = 'eb3f1faf1f54dacd424d662f5409be559231250762a39387e807bfcea57addccbb4fc98f97fd68e14a3ba932a0370188f8051779aca4b61c72e183ce120b174b'
